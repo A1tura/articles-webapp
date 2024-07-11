a9f6f5e555b7a5b3eb3182def78bf66ae9850797
A1tura <plandik275@gmail.com>
2024-07-11 21:17:49 +0200

    init: db

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import app from "./app";
 import dotenv from "dotenv";
 
 import {Server, IncomingMessage, ServerResponse} from "http"
+import { connect } from "./db/db";
 
 dotenv.config({path: "../.env"});
 
 
-function bootServer(): Server<typeof IncomingMessage, typeof ServerResponse> {
+async function bootServer(): Promise<Server<typeof IncomingMessage, typeof ServerResponse>>{
+
+    await connect();
+
     return app.listen(process.env["PORT"], () => {
         console.log("Listening at port " + process.env["PORT"]);
     })
