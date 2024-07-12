b4928e973709abff9c0bfaa94dc085d2faba7818
A1tura <plandik275@gmail.com>
2024-07-12 16:12:35 +0200

    implemented: jwt token verification

---
+++ src/middlewares/jwt.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config({path: "../.env"});
+
+export default function verificateJWT(req: Request, res: Response, next: NextFunction): void | Response {
+
+    const token = req.headers["authorization"]?.split(" ");
+
+    if (token?.length != 2) {
+        return res.json({success: false, errors: ["Auth failure: Invalid JWT token"]}).status(401);
+    }
+
+    if (token[0] != "Bearer") {
+        return res.json({success: false, errors: ["Auth failure: Invalid JWT token"]}).status(401);
+    }
+
+    try {
+        // refactor
+        const decoded = jsonwebtoken.verify(token[1], process.env["JWT_SECRET"] || "ff");
+
+        req.user = decoded;
+    } catch (err) {
+        return res.json({success: false, errors: err}).status(401);
+    }
+
+    next();
+}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {test, signup, signin, verifEmail} from "../controllers/user.controller";
 
+import jwtVerif from "../middlewares/jwt.middleware";
+
 const router = Router();
 
-router.get("/test", test);
+router.get("/test", jwtVerif ,test);
 router.post("/signup", signup)
 router.post("/signin", signin)
 router.get("/verifEmail/:token", verifEmail)
