f101949785c6de3ae400bd8628fcc2d5f6589021
A1tura <plandik275@gmail.com>
2024-07-13 13:48:53 +0200

    refactored: articles routes

--- src/routes/article.route.ts
+++ src/routes/article.route.ts
@@ -6,6 +6,6 @@ import jwtVerif from "../middlewares/jwt.middleware"
 const router = Router();
 
 router.get("/test", jwtVerif, test);
-router.post("/createArticle", jwtVerif, createArticle);
+router.post("/create", jwtVerif, createArticle);
 
 export default router;
